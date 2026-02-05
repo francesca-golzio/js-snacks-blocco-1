3bf65a2279206b916f771dbf491c39e2a42dfc34
Francesca Golzio <francescagolzio@gmail.com>
2026-02-05 11:37:47 +0100

    add snack 2

---
+++ snack 2/assets/JS/snack_2_main.js
@@ -0,0 +1,25 @@
+// L’utente inserisce due parole in successione, con due prompt.
+// Il software stampa prima la parola più corta, poi la parola più lunga. 
+
+/* Ask User to type 2 words (two prompst one for each word) */
+/* Save answers in a var */
+
+const word_1 = prompt('Type a word');
+const word_2 = prompt('Type another word');
+console.log(word_1, word_2);
+
+
+/* Compare strings length */
+let message = ''
+if(word_1.length > word_2.length) {
+  message = '"' + word_1 + '"' + ' is longer than ' + '"' + word_2 + '"';
+} else if(word_1.length < word_2.length) {
+  message = word_2 + ' is longer than ' + word_1;
+} else {
+  message = 'The two words have the same length'
+}
+
+/* Print the longest word, then print the shortest one */
+
+console.log(message);
+
